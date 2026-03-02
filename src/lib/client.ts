bede858ef04b442ee26ef15d06bfbcd4b9683a7f
GauravSinghdev <gauravkalakoti19@gmail.com>
2026-03-02 20:12:20 +0530

    adding domain-name

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,7 +2,7 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@/app/api/[[...slugs]]/route";
 
-export const client = treaty<App>("localhost:3000").api;
+export const client = treaty<App>("https://groupe.codewithkara.com").api;
 
 // typesafety for api routes -->
 // const res = client.user.get()
